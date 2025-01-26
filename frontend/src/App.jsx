cb8b82b2e9a34d346aa4f4bb411f03f64d2dd842
Sachin Mani Tripathi <sachintripathi1605@gmail.com>
2025-01-26 15:27:10 +0530

    file path issue resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom"
-import Home from "./pages/home/home"
+import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
 import SignUp from "./pages/signup/SignUp"
 import { Toaster} from "react-hot-toast"
